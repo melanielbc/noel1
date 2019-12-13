ed4af90b0b8f2b6c90508ab21926fa019a47554b
Mélanie Leblanc <themelanieleblanc99@gmail.com>
2019-12-13 12:14:38 +0100

    commit gerenal

--- includes/contact.inc.php
+++ includes/contact.inc.php
@@ -1,11 +1,57 @@
 <h1>Pour nous contacter</h1>
 <?php
 if (isset($_POST['frmContact'])) {
-  echo "Je viens du formulaire";
+  $nom = checkInput($_POST['nom']);
+  $prenom = checkInput($_POST['prenom']);
+  $mail = checkInput($_POST['mail']);
+  $msg = checkInput($_POST['msg']);
+
+  $erreur = array();
+
+  if ($nom === "")
+    array_push($erreur, "Veuillez saisir votre nom");
+
+  if ($prenom === "")
+    array_push($erreur, "Veuillez saisir un prénom");
+
+  if ($mail === "")
+    array_push($erreur, "Veuillez saisir votre une adresse mail");
+
+  if ($msg === "")
+    array_push($erreur, "Veuillez saisir un message");
+
+  if(count($erreur) > 0) {
+    $message = '<ul>';
+
+    for($i = 0 ; $i < count($serreur) ; $i++) {
+      $message .= '<li>';
+      $message .= $erreur[$i];
+      $message .= '</li>';
+    }
+
+    $message .= '</ul>';
+
+    echo $message;
+
+    require 'frmContact.php';
+  }
+
+  else {
+    $sql = "INSERT INTO clients
+    (nom, prenom, mail, message)
+    VALUES ('" . $nom . "', '" . $prenom . "', '" . $mail . "', '" . $msg . "')";
+
+    $query = $pdo->prepare($sql);
+   $query->bindValue('nom', $nom, PDO::PARAM_STR);
+   $query->bindValue('prenom', $prenom, PDO::PARAM_STR);
+   $query->bindValue('mail', $mail, PDO::PARAM_STR);
+   $query->bindValue('message', $msg, PDO::PARAM_STR);
+   $query->execute();
+   echo "ENregistrement OK";
+ }
 }
 
 else {
-  echo "Je viens du futur";
+  $nom = $prenom = $mail = $msg = "";
+  require 'frmContact.php';
 }
-
-require 'frmContact.php';
